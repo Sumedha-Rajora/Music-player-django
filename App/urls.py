74834be24e4c97324b7e8a6752b58f360264f70c
Sumedha-Rajora <sumedharajora@gmail.com>
2026-05-13 22:20:22 +0530

    fixed media urls

--- App/urls.py
+++ App/urls.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 from django.urls import path, include
 from . import views
 
@@ -10,6 +12,3 @@ urlpatterns = [
 
 
 ]
-
-if settings.DEBUG is False:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MusicPlayer/urls.py
+++ MusicPlayer/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.staticfiles.urls import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("App.urls")),
-
-
+    path('', include('App.urls')),
 ]
-urlpatterns += staticfiles_urlpatterns()
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
