2122b609d10aad30beab31522168076580344d68
Sumedha-Rajora <sumedharajora@gmail.com>
2026-05-13 23:17:34 +0530

    moved media to static

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'dj38dj3j3j3j3jd83j38djd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
