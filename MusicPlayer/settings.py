52aea05e4561f19b2f6c49d6dd8f11d01f85a3d5
Sumedha-Rajora <sumedharajora@gmail.com>
2026-05-14 13:37:43 +0530

    moved media to static

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dj38dj3j3j3j3jd83j38djd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
